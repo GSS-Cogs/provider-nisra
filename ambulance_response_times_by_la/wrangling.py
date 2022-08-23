384e820f5359ff3c4363a4e868dc27b5c63d6071
Andrew Fergusson <andrew.fergusson@ons.gov.uk>
2022-08-23 11:17:30 +0100

    finished wrangle.py

--- ambulance_response_times_by_la/wrangling.py
+++ ambulance_response_times_by_la/wrangling.py
@@ -1,16 +1,22 @@
-from pathlib import Path
 import click
-import pandas as pd 
+import pandas as pd
+
 
 @click.command()
-@click.argument('--input', type=click.Path, help='File to wrangle')
-def wrangle(input: Path)->None:
+@click.argument("input", type=click.Path(exists=True))
+@click.argument("output", type=click.Path())
+def wrangle(input: click.Path, output: click.Path) -> None:
     df = pd.read_csv(input)
-    #00:07:22
-    df['VALUE'].str.split(':').apply(lambda x: (int(x[0]) * 60) + int(x[1])+(int(x[2])/60))
-    df.to_csv("output.csv", index=False)
-    return 
+    # 00:07:22
+    df["VALUE"] = (
+        df["VALUE"]
+        .str.split(":")
+        .apply(lambda x: (int(x[0]) * 60) + int(x[1]) + (int(x[2]) / 60))
+        .round(decimals=2)
+    )
+    df.to_csv(output, index=False)
+    return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     wrangle()
-
