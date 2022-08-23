ec72eb33297e71f5b23bed53ca10756d65ca3768
Shannon Black <shannon1995black@gmail.com>
2022-08-23 10:01:20 +0000

    updates

--- ambulance_response_times_by_la/wrangling.py
+++ ambulance_response_times_by_la/wrangling.py
@@ -2,15 +2,15 @@ from pathlib import Path
 import click
 import pandas as pd 
 
-def wrangle(input: Path)->pd.DataFrame():
+@click.command()
+@click.argument('--input', type=click.Path, help='File to wrangle')
+def wrangle(input: Path)->None:
     df = pd.read_csv(input)
     #00:07:22
-    df["VALUE"] = int(df["VALUE"][0:1])*60 + int(df["VALUE"][3:4]) + int(df.iloc["VALUE"][6:8])/60
-
-    #df['VALUE'].apply(lambda x: x...)
-
-    return df
+    df['VALUE'].str.split(':').apply(lambda x: (int(x[0]) * 60) + int(x[1])+(int(x[2])/60))
+    df.to_csv("output.csv", index=False)
+    return 
 
 if __name__ == '__main__':
-    wrangle(Path("ARTLGD.20220816T100828.csv"))
+    wrangle()
 
